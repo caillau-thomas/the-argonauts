e8a539172881255ae28dddd48e9aba000ddcf182
alexis-mateo <alexis.mateo@etu.umontpellier.fr>
2024-10-29 01:21:59 +0100

    Add styled component to the project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import StyledComponentsRegistry from '@/lib/registry'
+
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -21,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
   );
