279301d05cb567b5cb51ecc73582801d2c28a15a
caillau-thomas <thomas.caillau@gmail.com>
2024-11-04 19:05:58 +0100

    feat: Ajout du components descriptionsCards

---
+++ src/app/components/cards/descriptionsCards/descriptionsCards.js
@@ -0,0 +1,14 @@
+// components/DescriptionsCards.js
+import React from 'react';
+import styles from './descriptionsCards.module.css';
+
+const DescriptionsCards = ({ title, description, style }) => {
+  return (
+    <div className={styles.card} style={style}>
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default DescriptionsCards;
