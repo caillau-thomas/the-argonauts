d77d57ed08d5c67f27fd25e4c71a0af5248f84c0
caillau-thomas <thomas.caillau@gmail.com>
2025-01-23 19:30:25 +0100

    agn-17 (style, feat): Update des differents styles, ajouts des carte de section, update du theme provider.

---
+++ src/Components/CardStyledComponents.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const SectionCard = styled.div`
+  background-color: var(--color-primary);
+  padding: var(--space-m);
+  background-color: var(--color-primary);
+  flex: 1;
+  height: 30vh; 
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.210); /* Ombre subtile */
+  transition: all 0.25s ease-in-out;
+`
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,21 +1,102 @@
 import styled from 'styled-components'
 
+import { SectionCard } from '../../Components/CardStyledComponents'
+
+const Body = styled.div``
+
 const LandingSection = styled.div`
-  background-color: red;
-  background-image: url('https://image.noelshack.com/fichiers/2025/04/3/1737581775-imgsc.jpg'); 
-  background-size: cover; 
-  background-position: center; 
-  background-repeat: no-repeat; 
-  height: 100vh;
   display: flex;
-  align-items: center; 
+  align-items: center;
   justify-content: center;
+  height: 100vh;
+
+  background-image: url("https://image.noelshack.com/fichiers/2025/04/3/1737581775-imgsc.jpg");
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+`
+
+const SectionsTitle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: var(--space-1xs);
+
+  background-color: var(--color-primary);
+`
+
+
+const SectionSection = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: var(--space-m);
+  padding: var(--space-l);
+  flex-wrap: wrap;
+
+  background-color: var(--color-primary);
+
+  &:has(.SectionCard:hover)
+  .SectionCard:not(:hover){
+    opacity:0.4;
+  }
+
+  @media (max-width: 1024px) {
+    flex-direction: column;
+  }
 `
 
 export const Home = () => {
   return (
-    <LandingSection>
-      <h1>Bienvenue</h1>
-    </LandingSection>
+    <Body>
+      <LandingSection>
+        <h1>
+          ARGONAUTS
+          <br />
+        </h1>
+
+        <h2>AU-DELÀ DU VERS</h2>
+      </LandingSection>
+      <SectionsTitle id='Sections'>
+        <h1>NOS SECTIONS</h1>
+      </SectionsTitle>
+      <SectionSection >
+        <SectionCard className='SectionCard'>
+          <h1>SECTION INDUSTRIEL & LOGISTIQUE</h1>
+          <p>
+            À LA RECHERCHE DE CHALLENGES LOGISTIQUES ET DE DÉFIS INDUSTRIELS,
+            NOUS ASSURONS L' AUTOSUFFISANCE DES ARGONAUTS ET BÂTISSONS LEUR
+            AVENIR.
+          </p>
+          <p>
+            EXTRAIRE DES RESSOURCES CONDUIRE LES CHAINES LOGISTIQUES BATIR NOS
+            INFRASTRUCTURES
+          </p>
+        </SectionCard>
+        <SectionCard className='SectionCard'>
+          <h1>SECTION DE REPONSE A INCIDENT</h1>
+          <p>
+            À LA RECHERCHE D'ADRÉNALINE ET D'EXCELLENCE, 
+            NOUS ASSURONS, SI NECESSAIRE AU PERIL DE NOS VIES, 
+            LA SÉCURITÉ DES NÔTRES AINSI QUE
+            CELLE DES CITOYENS DU VERSE.
+          </p>
+          <p>
+            ESCORTER NOS CONVOIS REPONDRE AUX BALISE DE 
+            DÉTRESSE DEFENDRE NOS ALLIÉS
+          </p>
+        </SectionCard>
+        <SectionCard className='SectionCard'>
+          <h1>SECTION DE SOUTIEN OPÉRATIONNEL</h1>
+
+          <p>
+            NOUS ASSISTONS LES DIFFÉRENTES SECTIONS DES ARGONAUTS SUR LES PLANS
+            TECHNIQUE ET MÉDICAL, AFIN D’ASSURER LEUR BON FONCTIONNEMENT.
+          </p>
+          <p>ASSURER LA MAINTENANCE DE NOTRE FLOTTE SECOURS AUX BLESSES</p>
+        </SectionCard>
+      </SectionSection>
+    </Body>
   )
 }
