4c2ec27dbe6fa737f9cf6e63e48b98d309a60eab
caillau-thomas <thomas.caillau@gmail.com>
2025-01-29 19:00:48 +0100

    agn-17 (style)

--- src/Components/CardStyledComponents.jsx
+++ src/Components/CardStyledComponents.jsx
@@ -1,5 +1,33 @@
 import styled from 'styled-components'
 
+export const AboutCard = styled.div`
+  padding: var(--space-m);
+  text-align: center;
+  flex: 1;
+  height: 30vh; 
+`
+
+export const SectionCard = styled.div`
+  flex: 1;
+  min-height: 20vh; 
+  width: 80%;
+  padding: var(--space-m);
+  border-radius: 16px;
+
+  background-color: var(--color-background); 
+  color: var(--color-primary);
+  border: 2px solid var(--color-border); 
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.210);
+  transition: all 0.25s ease-in-out;
+`
+export const HeadingCard = styled.div`
+  width: 100%;
+  text-align: left;
+`
+
+export const SectionsCardHeading = styled.div`
+  font-size: xx-large;
+`
 
 export const ContentCard = styled.div`
   display: flex;
@@ -7,27 +35,12 @@ export const ContentCard = styled.div`
   justify-content: center;
   gap: var(--space-m);
   flex-wrap: wrap;
+  margin: var(--space-l);
+
   color: var(--color-secondary);
   
   @media (max-width: 1024px) {
     flex-direction: column;
   }
-`
-
-export const SectionCard = styled.div`
-  background-color: var(--color-primary);
-  padding: var(--space-m);
-  flex: 1;
-  height: 60vh; 
-  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.210);
-  transition: all 0.25s ease-in-out;
-  border-radius: 4px;
-`
-
-export const AboutCard = styled.div`
-  padding: var(--space-m);
-  text-align: center;
-  flex: 1;
-  height: 30vh; 
 `
 
